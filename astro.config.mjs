9fd64466376aaaed6b87c4d2fd7f6f2c2a0fcaed
unknown <narcisal99325@gmail.com>
2026-05-03 03:26:07 +0800

    Change side bar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,20 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Narcisal Blog',
-	  components: {
-      customCss: ['./src/styles/global.css'],
-  	  },
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/Narcisal' },
       ],
+      header: {
+        links: [
+          { label: '日記', href: '/diary/diary-2026-05-03' },
+          { label: '工作筆記', href: '/notes/first-note' },
+          { label: '紀事', href: '/events/first-event' },
+        ],
+      },
       sidebar: [
-        {
-          label: '日記',
-          autogenerate: { directory: 'diary' },
-        },
-        {
-          label: '工作筆記',
-          autogenerate: { directory: 'notes' },
-        },
-        {
-          label: '紀事',
-          autogenerate: { directory: 'events' },
-        },
+        { label: '日記', autogenerate: { directory: 'diary' } },
+        { label: '工作筆記', autogenerate: { directory: 'notes' } },
+        { label: '紀事', autogenerate: { directory: 'events' } },
       ],
     }),
   ],
