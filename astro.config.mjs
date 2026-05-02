276159160f6f869299be7cab5b4fa0a689598d4f
unknown <narcisal99325@gmail.com>
2026-05-03 02:27:33 +0800

    Modify index.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Narcisal Blog',
-			description: 'Hello, I am Narcisal. Currently studying CS in NCKU.',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Narcisal' }],
-			sidebar: [
-				{
-					label: '日記',
-					autogenerate: { directory: 'diary' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Narcisal Blog',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/Narcisal' },
+      ],
+      sidebar: [
+        {
+          label: '日記',
+          autogenerate: { directory: 'diary' },
+        },
+        {
+          label: '工作筆記',
+          autogenerate: { directory: 'notes' },
+        },
+        {
+          label: '紀事',
+          autogenerate: { directory: 'events' },
+        },
+      ],
+    }),
+  ],
 });
